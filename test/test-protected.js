8f5f3ea2db605028fcdb660cc5a17d4c412dda57
Vicky <vyue013@gmail.com>
2018-10-02 22:50:17 -0700

    added auth protected tests

---
+++ test/test-protected.js
@@ -0,0 +1,119 @@
+"use strict";
+
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const jwt = require("jsonwebtoken");
+
+const { app, runServer, closeServer } = require("../server");
+const { User } = require("../users");
+const { JWT_SECRET, TEST_DATABASE_URL } = require("../config");
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe("Protected endpoint", function() {
+  const email = "admin@test.com";
+  const password = "pass123123";
+
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  beforeEach(function() {
+    return User.hashPassword(password).then(password =>
+      User.create({
+        email,
+        password
+      })
+    );
+  });
+
+  afterEach(function() {
+    return User.deleteMany({});
+  });
+
+  // describe("/api/protected", function() {
+  //   it("Should reject requests with no credentials", function() {
+  //     return chai
+  //       .request(app)
+  //       .get("/api/protected")
+  //       .then(res => {
+  //         expect(res).to.have.status(401);
+  //       });
+  //   });
+  //
+  //   it("Should reject requests with an invalid token", function() {
+  //     const token = jwt.sign(
+  //       {
+  //         email
+  //       },
+  //       "wrongSecret",
+  //       {
+  //         algorithm: "HS256",
+  //         expiresIn: "7d"
+  //       }
+  //     );
+  //
+  //     return chai
+  //       .request(app)
+  //       .get("/api/protected")
+  //       .set("Authorization", `Bearer ${token}`)
+  //       .then(res => {
+  //         expect(res).to.have.status(401);
+  //       });
+  //   });
+  //   it("Should reject requests with an expired token", function() {
+  //     const token = jwt.sign(
+  //       {
+  //         user: {
+  //           email
+  //         },
+  //         exp: Math.floor(Date.now() / 1000) - 10 // Expired ten seconds ago
+  //       },
+  //       JWT_SECRET,
+  //       {
+  //         algorithm: "HS256",
+  //         subject: email
+  //       }
+  //     );
+  //
+  //     return chai
+  //       .request(app)
+  //       .get("/api/protected")
+  //       .set("authorization", `Bearer ${token}`)
+  //       .then(res => {
+  //         expect(res).to.have.status(401);
+  //       });
+  //   });
+    it("Should send protected data", function() {
+      const token = jwt.sign(
+        {
+          user: {
+            email
+          }
+        },
+        JWT_SECRET,
+        {
+          algorithm: "HS256",
+          subject: email,
+          expiresIn: "7d"
+        }
+      );
+
+      return chai
+        .request(app)
+        .get("/api/protected")
+        .set("authorization", `Bearer ${token}`)
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("object");
+          expect(res.body.data).to.equal("rosebud");
+        });
+    });
+  });
+});
