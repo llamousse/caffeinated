577cb93ef4e24ffa9e9095831592df7cdc0a0c48
Vicky <vyue013@gmail.com>
2018-11-12 01:26:50 -0800

    added nav arrow

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -3,26 +3,26 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class ArrowDown extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    const currentState = this.state.active;
-    this.setState({
-      active: !currentState
-    })
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     active: false,
+  //   };
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
+  //
+  // handleClick() {
+  //   const currentState = this.state.active;
+  //   this.setState({
+  //     active: !currentState
+  //   })
+  // }
 
   render() {
     return (
-        <div className="arrowDown">
-          <a onClick={this.handleClick}>V</a>
-         </div>
-    )
+      <div className="arrowDown">
+        <a href="#discoverShops" className="discoverTag">V</a>
+      </div>
+    );
   }
 }
--- src/components/DiscoverShops/index.js
+++ src/components/DiscoverShops/index.js
@@ -7,7 +7,7 @@ import './index.css';
 export class DiscoverShops extends React.Component {
   render() {
     return (
-      <div className="discoverContainer">
+      <div id="discoverShops" className="discoverContainer">
         <h2 className="discoverHeader">Discover Local Coffee & Tea, and </h2>
         <h2 className="discoverHeaderBot">Bubble Tea Shops in the Area</h2>
 
